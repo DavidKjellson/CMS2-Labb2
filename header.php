b88d229800aab63db240ea5b763a31ca551b1b5a
David Kjellson <david.kjellson@gmail.com>
2020-10-28 15:49:17 +0100

    Removed images

--- header.php
+++ header.php
@@ -7,3 +7,9 @@
 </head>
 
 <body <?php body_class(); ?>>
+  <?php while (have_rows('startsida')) {
+    the_row(); ?>
+    <div class="jumbotron myhero" style="background-image:url('<?php echo get_sub_field('sidbild')['url']; ?>'); background-position: center;">
+      <h1 class="display-4 mydisplay" style="color: <?php echo get_sub_field('farg'); ?>;"><?php the_sub_field('sidtitel'); ?></h1>
+    </div>
+  <?php } ?>
